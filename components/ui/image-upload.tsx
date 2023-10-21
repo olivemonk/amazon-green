55efca73f533d250c681956c20c5237afabf89c4
OliveMonk <gp7.official@gmail.com>
2023-10-21 23:18:07 +0530

    preset changes

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -53,7 +53,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="t4drjppf">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="kliwxjq1">
         {({ open }) => {
           const onClick = () => {
             open();
